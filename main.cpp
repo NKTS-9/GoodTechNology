3b8163ab1268260dbf6eb4698e4c39f176356168
Darkness <47477957+DarknessDiAnjelo@users.noreply.github.com>
2021-03-26 19:39:20 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
 using namespace std;
 
 struct company
@@ -11,10 +13,42 @@ struct company
     int cost;
 }
 
+void upload(int &address)
+{
+    ifstream file("table.txt");
+    if(!file)
+    {
+        cout << "ERROR\nWE BREAK EVERETHING"
+    }
+
+}
+
 int main()
 {
     company tourism[10];
-    //fehui
+    int &address[10];
+    for (int i, i = 0; i++)
+    {
+        address[i] = *tourism[i];
+    }
+    int choice;
+    for(;;)
+    {
+        cout << "Выберите пункт меню:" << endl;
+        cout << "1. Загрузка файла\n2. Вывод на экран\n3. Редактирование\n4. Выход\n";
+        cin >> choice;
+        switch (choice)
+        {
+            case 1: Upload(); break;
+            case 2: Screen(); break;
+            case 3: Editting(); break;
+        }
+        if(a == 4)
+        {
+            break;
+        }
+
+    }
 
     return 0;
 }
