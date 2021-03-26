d3dd05ccd8203e1f13e7abb6e0e36cad00dc585c
Darkness <47477957+DarknessDiAnjelo@users.noreply.github.com>
2021-03-26 19:14:41 +0300

    Add files via upload
    Начало

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct company
+{
+    int ID;
+    string name;
+    string tours;
+    int duration;
+    int cost;
+}
+
+int main()
+{
+    company tourism[10];
+
+
+    return 0;
+}
